1615f3853d1ae371d7cf24b0caa9da860e19cb83
Tore Friis <torefriis@gmail.com>
2021-03-20 14:09:49 +0100

    Nu med den inverse matrix

--- gauss.py
+++ gauss.py
@@ -1,6 +1,8 @@
 import math
 import os
 
+
+# læser fil man har givet
 myFile = open('pythontext.txt')
 read = myFile.read()
 splitted1 = read.split('\n')
@@ -13,15 +15,26 @@ for i in range(len(splitted1)):
         splitted2[i][j] = int(splitted2[i][j])
 
 
-print("FILEN: \n", splitted2)
 
 # input section
 theinput = splitted2 # hver liste inde i listen er en række i en matrix.
 
-def operation(x, y, row, matrix, const): # x og y er kordinaterne for det punkt der skal laves om. row er den række som man tager udgangspunkt i. matrix er den matrix der skal bruges. y starter oppefra
-    out = matrix[y][x] - ((matrix[row][x])/(matrix[row][row]) * const)
-#    print("XY=", x, y, "row:", row, "was:", matrix[y][x], "is:", out)
-#    print(matrix)
+# danner enhedsmatrix
+enhedsMatrix = []
+for i in range(len(theinput)):
+    enhedsMatrix.append([])
+    # tilføj 0'er og 1'ere til enhedsmatricen
+    for j in range(len(theinput)):
+        if j==i:
+            enhedsMatrix[i].append(1)
+        else:
+            enhedsMatrix[i].append(0)
+enhedsMatrixNy = enhedsMatrix
+
+
+
+def operation(x, y, row, matrix, matrix1, const): # x og y er kordinaterne for det punkt der skal laves om. row er den række som man tager udgangspunkt i. matrix er den matrix der skal bruges. y starter oppefra
+    out = matrix1[y][x] - ((matrix1[row][x])/(matrix[row][row]) * const)
     return out
 
 # doing the things
@@ -29,10 +42,18 @@ for row in range(len(theinput)): # for hver række som er udgangspunkt
     for y in range(len(theinput)):
         if y != row:
             const = theinput[y][row]
+            const2 = enhedsMatrixNy[y][row]
             for x in range(len(theinput) + 1):
-                theinput[y][x] = operation(x, y, row, theinput, const)
+                theinput[y][x] = operation(x, y, row, theinput, theinput, const)
+                if x != (len(theinput)):
+                    enhedsMatrixNy[y][x] = operation(x, y, row, theinput, enhedsMatrixNy, const)
                 
 
 # printing the things
 for i in range(len(theinput)):
     print(f'value #{i} is: {theinput[i][len(theinput)]/theinput[i][i]}')
+
+for i in range(len(enhedsMatrixNy)):
+    for j in range(len(enhedsMatrixNy)):
+        enhedsMatrixNy[i][j] /= theinput[i][i]
+print("Invers matrix", enhedsMatrixNy)
